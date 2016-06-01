b26becee79a4b66efec231f5fb6e706e3878e726
Richard W. Norton <rwnorton@thenavisway.com>
2016-06-01 11:08:57 -0700

    instaparse-compat sql92 bnf grammar

--- src/sql_datomic/parser.clj
+++ src/sql_datomic/parser.clj
@@ -2,6 +2,28 @@
   (:require [instaparse.core :as insta]))
 
 (def parser
-  (->> "resources/sql.bnf"
+  (->> #_"resources/sql.bnf" "resources/sql-92.instaparse.bnf"
        slurp
        insta/parser))
+
+;; (def transform-operator
+;;   {"+" +
+;;    "-" -
+;;    "*" *
+;;    "/" /})
+
+;; (defn transform-operation [f v]
+;;   (if (coll? v)
+;;     (apply f v)
+;;     (f v)))
+
+;; (def transform-options
+;;   {:expr identity
+;;    :vector vector
+;;    :number read-string
+;;    :operator transform-operator
+;;    :operation transform-operation})
+
+;; (defn parse [input]
+;;   (->> (parser input)
+;;        (insta/transform transform-options)))
