d197fcf1c50776c7a8fda8586ff4a5b0e03ad21a
Richard W. Norton <rwnorton@thenavisway.com>
2016-06-06 15:05:41 -0700

    pivot to more basic sql grammar

--- src/sql_datomic/parser.clj
+++ src/sql_datomic/parser.clj
@@ -1,8 +1,11 @@
 (ns sql-datomic.parser
-  (:require [instaparse.core :as insta]))
+  (:require [instaparse.core :as insta]
+            [clojure.zip :as zip]
+            [clojure.string :as str]))
 
 (def parser
-  (-> "resources/sql.bnf"
+  (-> "resources/sql-basic.bnf"
+      #_"resources/sql.bnf"
       slurp
       (insta/parser
        :input-format :ebnf
@@ -155,7 +158,70 @@
       (ex-info "unknown statement type" {:statement_type statement_type
                                          :ast ast}))))
 
+(defn descend-to [match-fn node]
+  (loop [n node]
+    (if (zip/end? n)
+      nil
+      (let [v (zip/node n)]
+        (if (match-fn v)
+          v
+          (recur (zip/next n)))))))
+
+(defn start-of-select? [node]
+  (and (vector? node)
+       (= (first node) :query_specification)
+       (string? (second node))
+       (= (str/lower-case (second node)) "select")))
+
+(defn descend-to-value-expression [node]
+  (descend-to (fn [n]
+                (and (vector? n)
+                     (= (first n) :value_expression_primary)))
+              node))
+
+(defn field-value [value-expr-node]
+  {:pre [(= (first value-expr-node) :value_expression_primary)]}
+  (let [key (get-in value-expr-node [1 0])]
+    (case key
+      :unsigned_value_specification :string-or-int-lit
+      :column_reference :column-name
+      (ex-info "unknown field" {:field key
+                                :node value-expr-node}))))
+
+(defn extract-string-literal [node]
+  (when-let [n (descend-to (fn [v]
+                             (and (vector? v)
+                                  (= (first v) :character_string_literal)))
+                           node)]
+    (:character_string_literal n)))
+
+(defn extract-identifier-body [node]
+  (when-let [n (descend-to (fn [v]
+                             (and (vector? v)
+                                  (= (first v) :identifier_body)))
+                           node)]
+    (:identifier_body n)))
+
+(defn extract-digits [node]
+  (when-let [n (descend-to (fn [v]
+                             (and (vector? v)
+                                  (= (first v) :identifier_body)))
+                           node)]
+    (:identifier_body n)))
+
+(defn field-value-column-name [node]
+  (let [col (descend-to (fn [v]
+                       (and (vector? v)
+                            (= (first v) :column_name)))
+                        node)]
+    ))
+
+(defn select-fields [select-node]
+  (into [] (remove #(= [:comma ","] %) select-node)))
+
 (defn simplify-select [ast]
+  (let [z (zip/vector-zip ast)
+        s (descend-to start-of-select? z)])
   :select)
 (defn simplify-delete [ast]
   :delete)
