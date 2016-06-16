ef03ba90c212a6b3cc4e01afd07d2715b8ea5c75
Richard W. Norton <rwnorton@thenavisway.com>
2016-06-16 10:08:15 -0700

    add test description messages

--- test/sql_datomic/parser_test.clj
+++ test/sql_datomic/parser_test.clj
@@ -5,37 +5,43 @@
             [clj-time.core :as tm]
             [clojure.instant :as inst]))
 
-(defmacro parsable? [stmt]
-  `(is (prs/good-ast? (prs/parser ~stmt))))
+(defmacro parsable?
+  ([stmt]     `(is (prs/good-ast? (prs/parser ~stmt))))
+  ([stmt msg] `(is (prs/good-ast? (prs/parser ~stmt)) ~msg)))
 
 (deftest parser-tests
   (testing "SELECT statements"
-    (parsable? "SELECT a_table.name FROM a_table")
-    (parsable? "SELECT a_table.name FROM a_table")
-    (parsable? "SELECT a_table.name, a_table.age FROM a_table")
-    (parsable? "SELECT a_table.* FROM a_table")
-    (parsable? "SELECT a_table.name FROM a_table, b_table")
+    (parsable? "SELECT a_table.name FROM a_table"
+               "allows simple queries")
+    (parsable? "SELECT a_table.name, a_table.age FROM a_table"
+               "allows multiple columns in select list")
+    (parsable? "SELECT a_table.* FROM a_table"
+               "allows qualified star in select list")
+    (parsable? "SELECT a_table.name FROM a_table, b_table"
+               "allows implicit cartesian product in from list")
     (parsable?
      "SELECT a_table.foo
       FROM   a_table
-     ")
+     " "tolerant of newlines")
     (parsable?
-     "SELECT a_table.foo,
-		a_table.bar,
-             a_table.baz
-      FROM   a_table
-     ")
+     "SELECT a_table.foo,\r
+		a_table.bar,\r
+             a_table.baz\r
+      FROM   a_table\r
+     " "tolerant of tabs and carriage returns")
     (parsable?
      "
          SELECT a_table.foo FROM   a_table
-     ")
-    (parsable? "SELECT a_table.name FROM a_table WHERE a_table.name = 'foo'")
+     " "tolerant of newlines at beginning and end")
+    (parsable? "SELECT a_table.name FROM a_table WHERE a_table.name = 'foo'"
+               "allows string literals")
     (parsable?
      "SELECT a_table.*, b_table.zebra_id
       FROM a_table
          , b_table
       WHERE a_table.id = b_table.a_id
-        AND b_table.zebra_id > 9000")
+        AND b_table.zebra_id > 9000"
+     "supports where clause conjoined by and")
     (parsable?
      "SELECT a_table.*, b_table.zebra_id
       FROM a_table, b_table
@@ -50,14 +56,15 @@
     (parsable?
      "SELECT a_table.*
       FROM a_table
-      WHERE a_table.foo <> 0")
+      WHERE a_table.foo <> 0"
+     "supports <> as binary comparison in where clauses")
     (parsable?
      "SELECT a_table.*, b_table.zebra_id
       FROM a_table
          , b_table
       WHERE a_table.id = b_table.a_id
         AND b_table.zebra_id != 0
-      ")
+      " "supports != as binary comparison in where clauses")
     (parsable?
      "select
                         a_table.*
@@ -68,7 +75,7 @@
       where
                         a_table.id = b_table.a_id
                  and    b_table.zebra_id > 0
-      ")
+      " "tolerant of lowercase res words and exotic whitespace formatting")
     (parsable?
      "SELECT a_table.*, b_table.zebra_id
       FROM a_table, b_table
@@ -78,50 +85,61 @@
      "SELECT a_table.*
       FROM a_table
       WHERE a_table.created_on BETWEEN DATE '2007-02-01'
-                                   AND DATE '2010-10-10'")
+                                   AND DATE '2010-10-10'"
+     "supports between with date literals")
     (parsable?
      "select foo.name
         from foo
        where foo.title = 'git-\\'r-dun maestro'
-         and foo.hired_on <= date '2000-01-01'")
+         and foo.hired_on <= date '2000-01-01'"
+     "allows string literals with embedded single quote")
     (parsable?
      "select foo.id, foo.name, foo.title
         from foo
        where foo.is_active = true
-         and foo.group_id >= 3")
+         and foo.group_id >= 3"
+     "supports boolean literals in where clause")
     (parsable?
      "select survey-request.email,
              survey-request.completion-status
         from survey-request
        where survey-request.sent-date between date '2014-04-01'
                                           and date '2014-05-01'
-     ")
+     "
+     "supports hyphenation in table/column identifiers")
     (parsable?
      "SELECT a_table.*
       FROM a_table
       WHERE a_table.created_on BETWEEN DATETIME '2007-02-01T10:11:12'
-                               AND #inst \"2010-10-10T01:02:03.001-07:00\"")
+                               AND #inst \"2010-10-10T01:02:03.001-07:00\""
+     "supports #inst literals used alongside datetime literal")
     (parsable?
      "select foo.* from foo
       where product.prod-id between 4000 and 6000 and
-            product.category <> :product.category/action")
+            product.category <> :product.category/action"
+     "supports namespaced keyword literals as value in where clause")
     (parsable?
-     "select foo.bar from foo where product.tag = :alabama-exorcist-family")
+     "select foo.bar from foo where product.tag = :alabama-exorcist-family"
+     "supports non-namespaced keyword literals as value in where")
     (parsable?
      "select foo.bar from foo where
         product.uuid between
               #uuid \"576073c3-24c5-4461-9b84-dfe65774d41b\"
-          and #uuid \"5760745a-5bb5-4768-96f7-0f8aeb1a84f0\"")
+          and #uuid \"5760745a-5bb5-4768-96f7-0f8aeb1a84f0\""
+     "supports #uuid literals")
     (parsable?
      "select foo.bar from foo where
-        product.url = #uri \"http://example.com/products/2290\"")
+        product.url = #uri \"http://example.com/products/2290\""
+     "supports #uri literals")
     (parsable?
      "select foo.bar, #bytes \"QURBUFRBVElPTiBVTlRPVUNIQUJMRVM=\"
-      from foo where product.prod-id between 2000 and 3000")
+      from foo where product.prod-id between 2000 and 3000"
+     "supports #bytes (byte array) literals in select list")
     (parsable?
      "select foo.bar from foo where
               :product/url = #uri \"http://example.com/products/2290\"
-          and :product/category = :product.category/documentary")
+          and :product/category = :product.category/documentary"
+     "supports namespaced keyword as column (attr) in where clause")
     (is (= (prs/parser
             "select :foo/bar from foo where :foo/quux = :xyzzy.baz/foo")
            [:sql_data_statement
@@ -150,7 +168,8 @@
         foo
       (name, age, balance, joined_on)
       VALUES
-      ('foo', 42, 1234.56, date '2016-04-01')"))
+      ('foo', 42, 1234.56, date '2016-04-01')"
+     "allows string, integral, float, and date literals as values"))
 
   (testing "UPDATE statements"
     (parsable?
@@ -169,7 +188,8 @@
     (parsable?
      "delete from products where products.actor = 'homer simpson'")
     (parsable?
-     "delete from drop_bear_attacks")
+     "delete from drop_bear_attacks"
+     "allows where clause to be optional")
     (parsable?
      "
      DELETE FROM
@@ -222,6 +242,7 @@
                                str
                                inst/read-instant-date))]
             }))
+
     ;; select product.prod-id from product where product.prod-id between 1 and 2 and product.title <> 'foo'
     (is (= (prs/transform
             [:sql_data_statement
@@ -244,6 +265,7 @@
             [(list :between {:table "product", :column "prod-id"} 1 10)
              (list :not= {:table "product", :column "title"} "foo")]}
            ))
+
     ;; select foo.* from foo where product.prod-id between 4000 and 6000 and product.category <> :product.category/action
     (is (= (prs/transform
             [:sql_data_statement
@@ -267,6 +289,7 @@
              '(:not=
               {:table "product", :column "category"}
               :product.category/action)]}))
+
     ;; select foo.bar from foo where product.tag = :alabama-exorcist-family
     (is (prs/transform
          [:sql_data_statement
